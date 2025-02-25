7ed0464ecd42ead8ac2beac581f5413a25a2ac38
YE39FN <arthurbaldoalves@gmail.com>
2025-02-25 16:34:57 -0300

    Aula de PHP com exemplos de concatenação de variáveis

---
+++ Aula 01/hola.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+            ECHO "Hello World!<br>";
+            echo "Hello World!<br>";
+            EcHo "Hello World!<br>";
+
+            $nome = "Arthur";
+            $sobrenome = "Baldo"; 
+            $numero = 10;
+
+            echo "${nome} <h1> ${sobrenome} </h1>";
+            echo $numero . "<p>" . $nome . " " . $sobrenome. "</p>";
+        ?>
+
+
+<!-- Melhor aula de PW -->
+<!-- Echo é o print (Echo = Print) -->
+    </body>
+</html>
