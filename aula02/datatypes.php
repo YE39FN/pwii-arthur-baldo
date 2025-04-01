3687eb855c394d99d4818b42741c644a6b0d61f6
YE39FN <arthurbaldoalves@gmail.com>
2025-04-01 16:29:33 -0300

    Criamos um arquivo para ensinar datatypes

---
+++ aula02/datatypes.php
@@ -0,0 +1,27 @@
+<?php
+    $x = "Hello World!";
+    $y = 'Hello World!';
+
+    var_dump($x);
+    echo "<br>";
+    var_dump($y);
+    echo "<br>";
+
+    $x = 12.3;
+
+    var_dump($x);
+    echo "<br>";
+
+
+    $y = ['Arthur', 'Heitor', 'André', 2],
+
+    var_dump($y);
+    echo "<br>";
+    echo $y[1];
+    echo "<br>";
+
+    $meuObj = new stdClass();
+    $meuObj->nome = "Heitor";
+    $meuObj->idade = 
+
+?>
