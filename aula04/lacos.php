30a4261ecee772cfde8633caad62c0c7fe72de98
Arthur Baldo Alves <arthurbaldoalves@gmail.com>
2025-04-29 16:27:17 -0300

    Aula sobre laços de repetição (For, While e Do While).

---
+++ aula04/lacos.php
@@ -0,0 +1,27 @@
+<?php
+    echo "Do While <br>";
+    $contador = 1;
+
+    while($contador < 6){
+        echo $contador,   "<br>";
+        $contador++;
+    }
+
+    echo "Do While <br>";
+    //do while
+
+    $contadorii = 6;
+
+    do {
+        echo $contadorii;
+        $contadorii++;
+    } while ($contadorii <6);
+
+    //For
+    echo "<br> For <br>";
+
+    for ($x = 0; $x <= 10; $x++){
+        echo "O número é: $x <br>";
+    }
+
+?>
