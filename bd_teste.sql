5f2ceb247fbddabeb7ef620f351a97133e0f4e82
Arthur Baldo Alves <arthurbaldoalves@gmail.com>
2025-10-07 16:37:09 -0300

    Banco de Dados da Pasta "teste"

---
+++ bd_teste.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE teste;
+
+use teste;
+
+create table dados(
+id int auto_increment primary key,
+nome varchar(250),
+idade varchar(250),
+cpf varchar (250)
+);
+
+select * from dados;
+
+
